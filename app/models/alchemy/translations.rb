06c09a22d78d60be9ac22952819f6c6c9a068cef
Malachi Gray <graymd.cfi@gmail.com>
2015-11-20 18:24:21 -0500

    change TRANSLATION_PREFIX to config var

--- app/models/alchemy/translations.rb
+++ app/models/alchemy/translations.rb
@@ -2,7 +2,7 @@ module Alchemy
   module Translations
     # we want to prepend all translated data bound four our translation engine
     # with this prefix for orgainizational/namespace reasons
-    TRANSLATION_PREFIX = 'cms'
+    TRANSLATION_PREFIX = ENV['TRANSLATION_PREFIX']
     #TRANSLATION_PREFIX = 'do_not_translate'
   end
 end
