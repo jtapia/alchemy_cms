d43dab8106515cca32329f5a2a92ed78b4255552
Malachi Gray <graymd.cfi@gmail.com>
2015-11-24 13:28:31 -0500

    change from translate to do_not_translate

--- app/models/alchemy/content/factory.rb
+++ app/models/alchemy/content/factory.rb
@@ -19,8 +19,7 @@ module Alchemy
         if (description = content_description(element, essence_hash)).blank?
           raise ContentDefinitionError, "No description found in elements.yml for #{essence_hash.inspect} and #{element.inspect}"
         else
-          translate = description['translate'] == false ? false : true
-          new(name: description['name'], element_id: element.id, translate: translate)
+          new(name: description['name'], element_id: element.id, skip_translate: description['translate'] == false)
         end
       end
 
